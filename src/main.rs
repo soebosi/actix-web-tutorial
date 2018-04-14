d87bc4e74c56ad3dfac0fbde45e4e69d5376e5f7
ebosi38051 <ebosi38051@gmail.com>
2018-04-15 01:19:45 +0900

    Use prefix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn index(req: HttpRequest) -> &'static str {
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    server::new(|| vec![
+        App::new()
+            .prefix("/app1")
+            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
+        App::new()
+            .prefix("/app2")
+            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
+        App::new().resource("/", |r| r.f(index))])
         .bind("127.0.0.1:8088")
         .expect("Can not bind to 127.0.0.1:8088")
         .run();
