0f6668d821e4cd6a3f6e3c7a84879ad78776bcd6
ebosi38051 <ebosi38051@gmail.com>
2018-04-15 01:38:06 +0900

    Use state

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest, HttpResponse};
 
-fn index(req: HttpRequest) -> &'static str {
-    "Hello world!"
+use std::cell::Cell;
+use actix_web::{server, App, HttpRequest, http};
+
+struct AppState {
+    counter: Cell<usize>,
+}
+
+fn index(req: HttpRequest<AppState>) -> String {
+    let count = req.state().counter.get() + 1;
+    req.state().counter.set(count);
+
+    format!("Request number: {}", count)
 }
 
 fn main() {
-    server::new(|| vec![
-        App::new()
-            .prefix("/app1")
-            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
-        App::new()
-            .prefix("/app2")
-            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
-        App::new().resource("/", |r| r.f(index))])
+    server::new(||
+        App::with_state(AppState{counter: Cell::new(0)})
+            .resource("/", |r| r.method(http::Method::GET).f(index)))
         .bind("127.0.0.1:8088")
         .expect("Can not bind to 127.0.0.1:8088")
         .run();
